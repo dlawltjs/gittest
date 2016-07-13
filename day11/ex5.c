ba01f1f30beec2cf1643b3eb9d324054e883e3d1
dlawltjs <jiesun4022@gmail.com>
2016-07-13 10:41:24 +0900

    ....

--- day11/ex5.c
+++ day11/ex5.c
@@ -1,36 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 #include "../mapEditor/map.h"
-
-void map_drawTile_trn()
-{
-
-}
 
 int main()
 {
-	_S_MAP_OBJECT screenBuffer;
-	map_init(&screenBuffer);
+	_S_MAP_OBJECT screenBuf;
+	map_init(&screenBuf);
+	map_new(&screenBuf,16,16);
 
-	map_new(&screenBuffer,16,16);
-
-	map_PutTile(&screenBuffer,5,5,4);
-	map_PutTile(&screenBuffer,4,5,4);
-	map_PutTile(&screenBuffer,3,5,4);
-
-
-
-
+	map_PutTile(&screenBuf,6,5,4);	
+	map_PutTile(&screenBuf,5,5,4);	
+	map_PutTile(&screenBuf,4,5,4);	
+	map_PutTile(&screenBuf,6,6,4);	
+	map_PutTile(&screenBuf,5,6,4);	
+	map_PutTile(&screenBuf,4,6,4);	
+	map_PutTile(&screenBuf,6,7,4);	
+	map_PutTile(&screenBuf,5,7,4);	
+	map_PutTile(&screenBuf,4,7,4);	
+	
 	_S_MAP_OBJECT humanObj;
 	map_init(&humanObj);
 	map_load(&humanObj,"human.dat");
-
-	puts("----------------");
-
-	//map_drawTile(&humanObj,5,5,&screenBuffer);
-	map_dump(&screenBuffer,Default_Tilepalette);
-
-
+	puts("\r\n-----------------------");
+	map_drawTile_trn(&humanObj,5,5,&screenBuf);
+	map_dump(&screenBuf,Default_Tilepalete);
 
 	return 0;
 }
--- mapEditor/map.c
+++ mapEditor/map.c
@@ -1,104 +1,98 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 #include "map.h"
 
-char Default_Tilepalette[] = {'.','#','@','/',92,'+','='};
 
+char Default_Tilepalete[] = {
+	'.','#','@','/',92,
+	'+','=','A','>','<',
+	'|','-','*' };
 
 void map_init(_S_MAP_OBJECT *pObj)
 {
 	pObj->m_pBuf = NULL;
 }
 
-void map_dump(_S_MAP_OBJECT *pObj, char *pTile_pal)
+void map_dump(_S_MAP_OBJECT *pObj,char *pTile_pal)
 {
-	/*
-	putTile(
+	/*putTile(
 			0,pObj->m_header.m_nHeight,
 			0,pObj->m_header.m_nWidth,
 			pObj->m_header.m_nWidth,
-			pObj->m_pBuf,ptile_pal);
-
+			pObj->m_pBuf,
+			pTile_pal	
+		   );
 	*/
-
-	for(int iy=0;iy<pObj->m_header.m_nHeight;iy++) {
-		for(int ix=0;ix<pObj->m_header.m_nWidth;ix++) {
-			putchar(pTile_pal[ pObj->m_pBuf[iy*pObj->m_header.m_nWidth + ix]]);
+	for(int iy=0;iy < pObj->m_header.m_nHeight;iy++) {
+		for(int ix=0;ix < pObj->m_header.m_nWidth;ix++) {
+			putchar(pTile_pal[ pObj->m_pBuf[iy* pObj->m_header.m_nWidth + ix]]);
 		}
 		printf("\r\n");
 	}
 }
 
-void map_new(_S_MAP_OBJECT *pObj, int nWidth, int nHeight)
+void map_new(_S_MAP_OBJECT *pObj,int nWidth,int nHeight)
 {
-	int nSize = nWidth * nHeight;
+	int nSize = nWidth*nHeight; 
+	
+	if(pObj->m_pBuf !=NULL) {free(pObj->m_pBuf); }
+	pObj->m_header.m_nWidth =  nWidth;// atoi(strtok(NULL," "));
+	pObj->m_header.m_nHeight = nHeight;//atoi(strtok(NULL," "));
+	//printf("%d \r\n",nSize);
+	pObj->m_pBuf = malloc( nSize);
 
-	if(pObj->m_pBuf != NULL) { free(pObj->m_pBuf); }
-	pObj->m_header.m_nWidth = nWidth; //atoi(strtok(NULL," "));
-	pObj->m_header.m_nHeight = nHeight; //atoi(strtok(NULL," "));
-	pObj->m_pBuf = malloc(nSize);
-
-	for(int i=0;i<nSize;i++) {
+	for(int i=0;i< nSize ;i++) {
 		pObj->m_pBuf[i] = 0;
+		//printf("%d",pObj->m_pBuf[i]);
 	}
-
-
+	
 
 }
 
-
-void map_PutTile(_S_MAP_OBJECT *pObj, int x, int y, int nTileIndex)
+void map_PutTile(_S_MAP_OBJECT *pObj, int x,int y,int nTileIndex)
 {
-	pObj->m_pBuf[ pObj->m_header.m_nWidth * y + x ] = nTileIndex;
-
+	pObj->m_pBuf[ pObj->m_header.m_nWidth * y + x  ] = nTileIndex;
 }
 
-int map_save(_S_MAP_OBJECT * pObj, char *filename) //0:성공 1:실패
+int map_save(_S_MAP_OBJECT *pObj,char *filename) //0:성공 1:실패
 {
 	FILE *pf = fopen(filename,"wb");
-	fwrite( &(pObj->m_header),sizeof(pObj->m_header),1,pf);
-
+	fwrite( &( pObj->m_header),sizeof(pObj->m_header),1,pf);
 	int nSize = pObj->m_header.m_nWidth * pObj->m_header.m_nHeight;
 	fwrite(pObj->m_pBuf,nSize,1,pf);
 
 	fclose(pf);
-
 	return 0;
 
 }
 
-int map_load(_S_MAP_OBJECT *pObj, char *filename )
+int map_load(_S_MAP_OBJECT *pObj,char *filename) //0:성공 1:에러
 {
-	FILE *pf = fopen(filename,"rb");			
-	fread( &(pObj->m_header),sizeof(_S_MAP_HEADER),1,pf);	
+	FILE *pf = fopen(filename,"rb");
+	fread( &(pObj->m_header),sizeof(_S_MAP_HEADER),1,pf);
 
 	if(pObj->m_pBuf) {
 		free(pObj->m_pBuf);
-	}	
+	}
 	int nSize = pObj->m_header.m_nWidth * pObj->m_header.m_nHeight;
-
-	pObj->m_pBuf = malloc(nSize);
+	pObj->m_pBuf = malloc( nSize);
 
 	fread(pObj->m_pBuf,nSize,1,pf);
 
-	fclose(pf);
 
 	return 0;
 }
 
-
-
-
 void map_drawTile(_S_MAP_OBJECT *pObj,int posx,int posy,_S_MAP_OBJECT *pTarget)
 {
-	//_S_MAP_OBJECT *pObj = &humanObj;
-	//_S_MAP_OBJECT *pTarget = &screenBuffer;
-	//int posx = 5;
-	//int posy = 5;
+//	_S_MAP_OBJECT *pObj = &humanObj;
+//	_S_MAP_OBJECT *pTarget = &screenBuf;
+//	int posx = 5;
+//	int posy = 5;
 
-	for(int iy=0;iy<pObj->m_header.m_nHeight;iy++) {
-		for(int ix=0;ix<pObj->m_header.m_nWidth;ix++) {
-
+	for(int iy=0;iy < pObj->m_header.m_nHeight;iy++) {
+		for(int ix = 0; ix < pObj->m_header.m_nWidth;ix++) {
 			map_PutTile(pTarget,ix + posx,iy + posy,
 					pObj->m_pBuf[iy*pObj->m_header.m_nWidth + ix]);
 		}
@@ -106,31 +100,42 @@ void map_drawTile(_S_MAP_OBJECT *pObj,int posx,int posy,_S_MAP_OBJECT *pTarget)
 
 }
 
-
-void map_drawTile_mirror_h(_S_MAP_OBJECT *pObj,int posx,int posy,_S_MAP_OBJECT *pTarget)
-{
-	for(int iy=0;iy<pObj->m_header.m_nHeight;iy++) {
-		for(int ix=0;ix<pObj->m_header.m_nWidth;ix++) {
-
-			map_PutTile(pTarget,ix - posx,iy + posy,
+void map_drawTile_mirror_h(_S_MAP_OBJECT *pObj,int posx,int posy,_S_MAP_OBJECT *pTarget) 
+{	
+	for(int iy=0;iy < pObj->m_header.m_nHeight;iy++) {
+		for(int ix = 0; ix < pObj->m_header.m_nWidth;ix++) {
+			map_PutTile(pTarget, posx - ix,iy + posy,
 					pObj->m_pBuf[iy*pObj->m_header.m_nWidth + ix]);
 		}
 	}
 
-
 }
 
-void map_drawTile_mirror_v(_S_MAP_OBJECT *pObj,int posx,int posy,_S_MAP_OBJECT *pTarget)
-{
-	for(int iy=0;iy<pObj->m_header.m_nHeight;iy++) {
-		for(int ix=0;ix<pObj->m_header.m_nWidth;ix++) {
 
-			map_PutTile(pTarget,ix - posx,iy + posy,
+void map_drawTile_mirror_v(_S_MAP_OBJECT *pObj,int posx,int posy,_S_MAP_OBJECT *pTarget) 
+{	
+	for(int iy=0;iy < pObj->m_header.m_nHeight;iy++) {
+		for(int ix = 0; ix < pObj->m_header.m_nWidth;ix++) {
+			map_PutTile(pTarget,ix + posx, posy -iy,
 					pObj->m_pBuf[iy*pObj->m_header.m_nWidth + ix]);
 		}
 	}
 
+}
+
+void map_drawTile_trn(_S_MAP_OBJECT *pObj, int posx,int posy,_S_MAP_OBJECT *pTarget) 
+{
+	for(int iy=0;iy < pObj->m_header.m_nHeight;iy++) {
+		for(int ix = 0; ix < pObj->m_header.m_nWidth;ix++) {
+			int nPixel = pObj->m_pBuf[iy*pObj->m_header.m_nWidth + ix];
+			if(nPixel != 0) {
+				map_PutTile(pTarget,ix + posx,iy + posy,
+						pObj->m_pBuf[iy*pObj->m_header.m_nWidth + ix]);
+			}
+		}
+	}
 
 }
+
 
 
