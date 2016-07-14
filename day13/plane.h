b38e5c8c178c7c5edbf54e59e0ad4d436cba7c2b
dlawltjs <jiesun4022@gmail.com>
2016-07-14 17:54:56 +0900

    ....

--- day13/plane.h
+++ day13/plane.h
@@ -1,10 +1,12 @@
 #ifndef __PLANE_H__
 #define __PLANE_H__
 
-typedef struct {
+typedef struct _S_Plane {
 	int m_nXpos;
 	int m_nYpos;
 	_S_MAP_OBJECT *m_pBody;
+	void (*fpApply)(struct _S_Plane *,double ,char );
+	void (*fpDraw)(struct _S_Plane *,_S_MAP_OBJECT *);
 } _S_Plane;
 
 void Plane_init(_S_Plane *pObj,_S_MAP_OBJECT *pBody,int x,int y);
